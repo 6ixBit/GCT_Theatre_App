5974816ca1a0f04cf9a5916f96c4aef38a173240
H <mn7754c@gre.ac.uk>
2019-01-20 02:15:07 +0000

    Added get and sets for user and tickets

--- src/Sys_Dev/src/sys_dev/Sys_Dev.java
+++ src/Sys_Dev/src/sys_dev/Sys_Dev.java
@@ -4,13 +4,32 @@ package sys_dev;
 
 public class Sys_Dev {
 
-    static Purchase p = new Purchase();
+    //Protected vars for user object - Each user will have one of these variables assigned to them when they sign up.
+    protected String user_name;
+    protected String pass_w;
+    protected String numb;
+    protected String address;
+    protected String postcode;
+    protected String e_mail;
     
-    private double shopping_cart = 0.0;
-    public static int discount = 10;
-  
-    public static void main(String[] args) {
-       p.run(discount);
+    //Protected vars for ticket attributes
+    protected int ticket_no;
+    protected String event_name;
+    protected String event_date;
+    protected double ticket_price;
+    
+    //Protected var for multiple tickets which should equal to a total price
+    protected double total_price;
+    
+    //protected var for discounts
+    protected double discount_1 = total_price / 100 * 10; //10% discount if user purchases a ticket between Monday - Thursday
+    
+    public Sys_Dev(){
+       int x = 400 / 100*10;
     }
     
+    public static void main(String[] args) {
+        Sys_Dev sav = new Sys_Dev();
+        System.out.println(sav);
+    }
 }
---
+++ src/Sys_Dev/src/sys_dev/Ticket.java
@@ -0,0 +1,38 @@
+package sys_dev;
+
+public class Ticket extends User {
+
+    public int get_ticket_no() {
+        return ticket_no;
+    }
+
+    public void set_ticket_no(int ticket) {
+        this.ticket_no = ticket;
+    }
+
+    public String get_event_name() {
+        return event_name;
+    }
+
+    public void set_event_name(String event) {
+        this.event_name = event;
+    }
+
+    public String get_event_date() {
+        return event_date;
+    }
+
+    public void set_event_date(String event) {
+        this.event_date = event;
+    }
+
+    public double get_ticket_price() {
+        return ticket_price;
+    }
+
+    public void set_ticket_price(double price) {
+        this.ticket_price = price;
+
+    }
+
+}
